0c8f9b5b7228a51b1200730a7db90f48d76dc6d3
Alex <alex@1200wd.com>
2018-04-20 14:07:04 +0200

    Refactoring stuff to a module per platform

--- protocolhandle.py
+++ protocolhandle.py
@@ -1,81 +1,24 @@
 import os
-import os.path
+import sys
 import argparse
 import re
 import base64
-import sys
-import subprocess
 
-try:
-    import _winreg as wr
-except ImportError:
-    import winreg as wr
+if sys.platform.startswith('darwin'):
+    import macosx as platform
+elif os.name == 'nt':
+    import windows as platform
+elif os.name == 'posix':
+    import linux as platform
+else:
+    raise Exception('OS not supported: {}'.format(os.name))
 
-ACCESS_RIGHTS = (wr.KEY_WRITE | wr.KEY_READ
-                | wr.KEY_QUERY_VALUE | wr.KEY_SET_VALUE
-                | wr.KEY_CREATE_SUB_KEY | wr.KEY_ENUMERATE_SUB_KEYS)
-
-
-def register_octosearch_protocol_handler():
-    dirname = os.path.dirname(os.path.abspath(__file__))
-    current_file = os.path.join(dirname, sys.argv[0])
-
-    command = current_file + ' ' + '"%1"'
-    protocol = 'octosearch'
-    description = 'Octosearch local file opener protocol handler'
-
-    print('Attempting to register protocol handler')
-    print('command = {}'.format(command))
-    register_protocol_handler(protocol, command, description)
-
-
-def register_protocol_handler(protocol, command, description):
-    r"""
-    HKEY_CLASSES_ROOT
-        foo
-            (Default) = "URL: Foo Protocol"
-            URL Protocol = ""
-            DefaultIcon
-                (Default) = "foo.exe,1"
-            shell
-                open
-                    command
-                        (Default) = "C:\Program Files\example\foo.exe" "%1"
-    """
-
-    with wr.CreateKeyEx(wr.HKEY_CLASSES_ROOT, protocol, 0, ACCESS_RIGHTS) as key:
-        wr.SetValueEx(key, "", 0, wr.REG_SZ, "URL: " + description)
-        wr.SetValueEx(key, "URL Protocol", 0, wr.REG_SZ, "")
-
-        shell_key = wr.CreateKeyEx(key, "shell", 0, ACCESS_RIGHTS)
-        open_key = wr.CreateKeyEx(shell_key, "open", 0, ACCESS_RIGHTS)
-        command_key = wr.CreateKeyEx(open_key, "command", 0, ACCESS_RIGHTS)
-
-        wr.SetValueEx(command_key, "", 0, wr.REG_SZ, command)
-
-
-def handle_open(url):
-    filepath = parse_url(url)
-    print('Opening {}...'.format(filepath))
-    open_file(filepath)
-
-
-def open_file(filepath):
-    if not isinstance(filepath, str):
-        raise Exception('filepath param must be str object')
-
-    if sys.platform.startswith('darwin'):
-        subprocess.call(('open', filepath))
-    elif os.name == 'nt':
-        os.startfile(filepath)
-    elif os.name == 'posix':
-        subprocess.call(('xdg-open', filepath))
-    else:
-        raise Exception('OS not supported: {}'.format(os.name))
+PROTOCOL = 'octosearch'
+VERSION = 'v1'
 
 
 def parse_url(url):
-    expr = r"octosearch://v1/open/(?P<encodedlocation>.+)"
+    expr = PROTOCOL + r"://" + VERSION + "/open/(?P<encodedlocation>.+)"
     match = re.fullmatch(expr, url)
 
     if not match:
@@ -89,6 +32,23 @@ def parse_url(url):
     return location.decode()
 
 
+def executable_location():
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    current_file = os.path.join(dirname, sys.argv[0])
+
+    return current_file
+
+
+def register_protocol_handler():
+    platform.register()
+
+
+def handle_open(url):
+    filepath = parse_url(url)
+    print('Opening {}...'.format(filepath))
+    platform.open_file(filepath)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Octosearch protocol handler')
     parser.add_argument('--register', dest='register', required=False, action='store_true', help='Register the protocol handler')
@@ -96,6 +56,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.register:
-        register_octosearch_protocol_handler()
+        register_protocol_handler()
     else:
         handle_open(args.url)
--- test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-import os.path
-
-print(os.path.abspath(__file__))
